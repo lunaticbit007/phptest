572fc51feafd0a760750c5f5e12402cfde3c8567
Ankush Pandey <51123683+ankushpandey001@users.noreply.github.com>
2019-05-29 22:28:19 +0530

    Update factor.php

--- src/php/factor.php
+++ src/php/factor.php
@@ -3,13 +3,18 @@
 function factorial( $n ) {
  
    $result = 1;
- 
-  if ( $n != 0 ) {
+ //check is it greater than zero 
+  if ( $n > 0 ) {
     for ($x=$n; $x>=1; $x--)   
     {  
     $result = $result * $x;  
     }  
   }
+ elseif($n < 0) // check negetive values
+ {
+  $result = 'Undefined';
+ }
+ 
   return $result;
 }
 
