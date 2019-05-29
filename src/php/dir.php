cc0783bcc0da05a49ebdf8bc882e2453c8bc03fa
Ankush Pandey <51123683+ankushpandey001@users.noreply.github.com>
2019-05-29 22:20:36 +0530

    Add files via upload

---
+++ src/php/dir.php
@@ -0,0 +1,57 @@
+<?php
+class ReadDirectory
+ {
+  public function read($dirName)
+   {
+       $directoriesArray = array();
+    if(is_dir($dirName))
+    {
+     if ($dh = opendir($dirName))
+     {
+	    //Open a directory, and read its contents
+      while (($file = readdir($dh)) !== false)
+      {
+       $directoriesArray[] = $file;
+      }
+       closedir($dh);
+     }
+    }
+    else
+    {
+     echo "Not A Directory";
+    }
+    return $directoriesArray;
+   }
+
+   public function sort($sortingData)
+   {
+    $n=count($sortingData);
+    $next=null;
+    for($i=1; $i<$n; $i++)//outer loop
+    {
+        $next=$sortingData[$i];
+        for($j=$i-1; $j>=0; $j--)//inner loop
+        {
+            if( $sortingData[$j]>$next ) 
+            {
+                $sortingData[$j+1]=$sortingData[$j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        $sortingData[$j+1]=$next; // insert the next value to the correct postion of the already sorted elements
+ 
+    
+    }
+    return $sortingData;
+   }
+ }
+
+$dirName = readline("Enter Directory Path : ");
+$object = new ReadDirectory(); 
+$directories = $object->read($dirName); //Call Method For Read the File and Sort It
+$sortedName = $object->sort($directories); // For sort the array
+
+print_r($sortedName);
