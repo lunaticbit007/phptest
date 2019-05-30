b72c73ed83c3ba9fe38aa992a9630528b59cff8b
Ankush Pandey <51123683+ankushpandey001@users.noreply.github.com>
2019-05-30 23:17:39 +0530

    Update dir.php

--- src/php/dir.php
+++ src/php/dir.php
@@ -1,53 +1,59 @@
 <?php
-class ReadDirectory
- {
-  public function read($dirName)
-   {
-       $directoriesArray = array();
-    if(is_dir($dirName))
-    {
-     if ($dh = opendir($dirName))
-     {
-	    //Open a directory, and read its contents
-      while (($file = readdir($dh)) !== false)
-      {
-       $directoriesArray[] = $file;
-      }
-       closedir($dh);
-     }
-    }
-    return $directoriesArray;
-   }
+/*
+@this class use for read the directory and files of the directory and sort it
+@read function read the file and put it in array
+@sort function sort the array which we provide
 
-   public function sort($sortingData)
-   {
-    $n=count($sortingData);
-    $next=null;
-    for($i=1; $i<$n; $i++)//outer loop
-    {
-        $next=$sortingData[$i];
-        for($j=$i-1; $j>=0; $j--)//inner loop
-        {
-            if( $sortingData[$j]>$next ) 
-            {
-                $sortingData[$j+1]=$sortingData[$j];
-            }
-            else
-            {
-                break;
+*/
+class ReadDirectory {
+
+  // read the directory
+    public function read($dirName) {
+     
+        $directoriesArray = array(); // Define array
+        //
+        if(is_dir($dirName)) {
+            if ($dh = opendir($dirName)) {
+      
+                //Open a directory, and read its contents
+                while (($file = readdir($dh)) !== false) {
+                    $directoriesArray[] = $file;
+                }
+                closedir($dh);
             }
         }
-        $sortingData[$j+1]=$next; // insert the next value to the correct postion of the already sorted elements
- 
-    
+        return $directoriesArray;
     }
-    return $sortingData;
-   }
- }
+    /* Use Insertion sort method for sorting the array 
+       $sortingData is array 
+    */
+    public function sort($sortingData) {
+        
+        $n=count($sortingData); // Count the value of array
+        $next=null; //definde the default value of next variable
+        //Outer Loop
+        for($i=1; $i<$n; $i++) {
+            $next=$sortingData[$i]; // Assigned the value of ith index of array
+            //Inner loop
+            for($j=$i-1; $j>=0; $j--){
+                // Check the values
+                if( $sortingData[$j] > $next ) {
+                    $sortingData[$j+1]=$sortingData[$j]; // 
+                }
+                else
+                {
+                break; // break the inner loop
+                }
+            }
+            $sortingData[$j+1]=$next; // insert the next value to the correct postion of the already sorted elements
+        }
+        return $sortingData;
+    }
+}
 
-$dirName = readline("Enter Directory Path : ");
-$object = new ReadDirectory(); 
-$directories = $object->read($dirName); //Call Method For Read the File and Sort It
-$sortedName = $object->sort($directories); // For sort the array
+$dirName = readline("Enter Directory Path : "); // For Insert the direcotry path
+$object = new ReadDirectory();                  // Create Object of the class
+$directories = $object->read($dirName);         //Call Method For Read the File and Sort It
+$sortedName = $object->sort($directories);      // For sort the array
 
-print_r($sortedName);
+print_r($sortedName); // Print All files and directories in sorted way
