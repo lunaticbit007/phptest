f2c9124a9b56497260076928ff44c107e6283696
Ankush Pandey <51123683+ankushpandey001@users.noreply.github.com>
2019-05-30 23:22:56 +0530

    Update factor.php

--- src/php/factor.php
+++ src/php/factor.php
@@ -1,20 +1,26 @@
 <?php
-
+    // function to get factorial  
 function factorial( $n ) {
-  // Base case
-  if ( $n <= 1 && $n >=0 ) { 
-    return 1;
+ 
+  $result = 1; //Assigned default value 1 
+      
+    //check is it greater than zero  
+  if ( $n > 0 ) {
+    for ( $x=$n ; $x>=1 ; $x-- ) {  // Loop for getting factorial data
+        $result = $result * $x;  
+    }  
+  } 
+  elseif($n < 0) { // check for negative values
+    $result = 'Undefined';  
   }
-  elseif($n < 0 ) // For negetive values
-  {
-    return "undefined";
-  }
-  // Recursion
-  $result = ( $n * factorial( $n-1 ) );
-  
-  return $result;
+ 
+  return $result; 
 }
 
-echo "The factorial of 5 is: " . factorial( 5 );
+echo "The factorial of 5 is: " . factorial( 5 ); // Call the function and print the factorial value
+
+/*
+NOTE : Use condition ($n < 0) for negative values because factorial of negative value is infinite, if we don't check this condition than our browser or terminal will get hang due to improper logic
+*/
 
 ?>
